a94c0b7da959b3fc245de630af025547ed16d676
miyabiGitHub <k.msy@outlook.jp>
2025-04-06 17:20:33 +0900

    案件登録画面作成

--- src/main/resources/static/js/app_main.js
+++ src/main/resources/static/js/app_main.js
@@ -8,6 +8,17 @@ function showContent(menu) {
 
     title.textContent = menu;
 
+    if (menu==="案件新規登録") {
+        fetch('register.html')
+        .then(response => response.text())
+        .then(html => {
+            contentArea.innerHTML = html;  // ← innerHTMLで埋め込む
+        })
+        .catch(error => {
+            console.error('読み込みエラー:', error);
+            contentArea.innerHTML = '<p>登録画面の読み込みに失敗しました。</p>';
+        });
+    }
     if (["案件情報一覧", "申請待ち", "承認済み", "差し戻し"].includes(menu)) {
         contentArea.innerHTML = `
             <ul id="data-list">
@@ -20,8 +31,22 @@ function showContent(menu) {
         contentArea.innerHTML = `<p>社員情報を登録するフォーム（仮）</p>`;
     } else if (menu === "ユーザ情報") {
         contentArea.innerHTML = `<p>ユーザ情報を登録するフォーム（仮）</p>`;
+    } else if (menu === "案件新規登録") {
+        contentArea.innerHTML= `
+            <p>ユーザ情報を登録するフォーム（仮）</p>
+        `;
     }
 }
+
+function toggleSubMenu() {
+    const submenu = document.getElementById('submenu');
+    if (submenu.style.display === 'none') {
+        submenu.style.display = 'block';
+    } else {
+        submenu.style.display = 'none';
+    }
+}
+
 
 function logout() {
     window.location.href = "index.html";
