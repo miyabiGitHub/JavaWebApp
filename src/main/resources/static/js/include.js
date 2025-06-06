79bce25301ae83b9c4c9b5eeb94f39128d339cd6
miyabiGitHub <k.msy@outlook.jp>
2025-06-06 21:56:37 +0900

    コード最適化ver2

--- src/main/resources/static/js/include.js
+++ src/main/resources/static/js/include.js
@@ -43,3 +43,39 @@ document.addEventListener("DOMContentLoaded", async () => {
             document.getElementById("pending").style.display = "none";
         }        
 });
+
+function toggleSubMenu1() {
+    const submenu1 = document.getElementById('submenu1');
+    if (submenu1.style.display === 'none') {
+        submenu1.style.display = 'block';
+    } else {
+        submenu1.style.display = 'none';
+    }
+}
+
+function toggleSubMenu2() {
+    const submenu2 = document.getElementById('submenu2');
+    if (submenu2.style.display === 'none') {
+        submenu2.style.display = 'block';
+    } else {
+        submenu2.style.display = 'none';
+    }
+}
+
+function toggleSubMenu3() {
+    const submenu3 = document.getElementById('submenu3');
+    if (submenu3.style.display === 'none') {
+        submenu3.style.display = 'block';
+    } else {
+        submenu3.style.display = 'none';
+    }
+}
+
+function toggleSubMenu4() {
+    const submenu4 = document.getElementById('submenu4');
+    if (submenu4.style.display === 'none') {
+        submenu4.style.display = 'block';
+    } else {
+        submenu4.style.display = 'none';
+    }
+}
